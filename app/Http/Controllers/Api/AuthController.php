b9afe56fbac0b3732b1a45448fdc40b528f89eac
Danesh <h_dansh@yahoo.com>
2024-07-11 17:22:23 +0330

    feat(api): user authentication (register, login, logout)

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+
+class AuthController extends Controller
+{
+
+
+    public function register(RegisterUserRequest $request)
+    {
+        $data = [
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => $request->password,
+        ];
+
+        $user = User::create($data);
+
+        $token = $user->createToken($request['email']);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'The provided credentials are incorrect'
+            ];
+        }
+
+        $token = $user->createToken($user->email);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return [
+            'message' => 'You are logged out.'
+        ];
+    }
+}
