c7045a1d266a3530fc0a4e20a1e46098cb124f12
Danesh <h_dansh@yahoo.com>
2024-07-12 15:45:17 +0330

    feat(api): implement profile.
    - view profile
    - delete profile
    - update profile (not completed yet)

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    public function show($id)
+    {
+        $profile = User::find($id);
+        return response()->json($profile);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //todo
+        // $profile = User::where('user_id', $id)->firstOrFail();
+        // $profile->update($request->all());
+        // return response()->json($profile);
+    }
+
+    public function destroy($id)
+    {
+        //todo check if auth->id == $id.
+        $profile = User::find($id);
+        $profile->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LikeController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ProfileController;
 
 //authentication user
 Route::post("/register", [AuthController::class, "register"]);
@@ -20,4 +21,10 @@ Route::prefix("posts")->group(function () {
     Route::post("/likes", [LikeController::class,"store"]);
     Route::delete("/likes", [LikeController::class,"destroy"]);
 
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('users/{user}/profile', [ProfileController::class, 'show']);
+    Route::put('users/{user}/profile', [ProfileController::class, 'update']);
+    Route::delete('users/{user}/profile', [ProfileController::class, 'destroy']);
 });
