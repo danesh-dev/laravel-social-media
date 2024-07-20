335d298b28cbd3f35363506edd5b1355fd0deb88
Danesh <h_dansh@yahoo.com>
2024-07-20 12:21:12 +0330

    fix: navbar account dropdown

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,33 +1,30 @@
-<nav class="navbar navbar-expand-lg navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#"><i class="fas fa-globe"></i> Social Media</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-        aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav ms-auto">
             @auth
-                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-plus-circle"></i> New Post</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-plus-circle"></i> New Post</a></li>
                 <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-envelope"></i> Messages</a></li>
-                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-home"></i> Home</a>
-                </li>
-                {{-- todo fix dropdown not opening --}}
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="true">
+                <li class="nav-item"><a class="nav-link" href="/"><i class="fas fa-home"></i> Home</a></li>
+                <li class="nav-item dropdown-center">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fas fa-user"></i> Account
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">my profile</a></li>
-                        <li><a class="dropdown-item" href="#">my posts</a></li>
-                        <li><a class="dropdown-item" href="#">edit account</a></li>
-                        <li><a class="dropdown-item" href="#">logout</a></li>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">My Profile</a></li>
+                        <li><a class="dropdown-item" href="#">My Posts</a></li>
+                        <li><a class="dropdown-item" href="#">Edit Account</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="/logout">Logout</a></li>
                     </ul>
                 </li>
             @else
                 <li class="nav-item"><a class="nav-link" href="{{ route('showLogin') }}"><i class="fas fa-sign-in-alt"></i> Login</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('showRegister') }}"><i class="fas fa-user-plus"></i> Register</a></li>
             @endauth
-            </ul>
-        </div>
-    </nav>
+        </ul>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- font awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
     @include('components.navbar')
 
     <div class="container">
         @yield('content')
     </div>
+
+    <!-- Popper.js, and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </body>
+
 </html>
