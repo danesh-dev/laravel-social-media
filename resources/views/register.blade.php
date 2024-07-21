a652d1e45c44c88bf2a3204f941f81858c8142e4
Danesh <h_dansh@yahoo.com>
2024-07-21 10:33:54 +0330

    fix: register form bio field

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -19,27 +19,32 @@
                     </ul>
                 </div>
             @endif
-            <div class="form-group">
-                <label for="name">Name</label>
+            <div class="form-group mt-1">
+                <label for="name">Name*</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" required>
             </div>
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username" required>
+            <div class="form-group mt-3">
+                <label for="username">Username*</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username"
+                    required>
             </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
+            <div class="form-group mt-3">
+                <label for="email">Email*</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email"
+                    required>
             </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
+            <div class="form-group mt-3">
+                <label for="password">Password*</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password"
+                    required>
             </div>
-            <div class="form-group">
+            <div class="form-group mt-3">
                 <label for="bio">Bio</label>
-                <input type="text" class="form-control" id="bio" name="bio" placeholder="Enter your biography" required>
+                <textarea class="form-control" id="bio" name="bio" placeholder="Enter your biography" required></textarea>
             </div>
-            <button type="submit" class="btn btn-primary btn-block mt-4">Register</button>
+            <div class="row position-relative col-10 mx-auto mt-2">
+                <button type="submit" class="btn btn-primary btn-block mt-4">Register</button>
+            </div>
         </form>
     @endcomponent
 @endsection
